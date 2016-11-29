3b43c2626fc38df51f5e6a098e0def89903d7fe4
23878410v <23878410v@iespoblenou.org>
2016-11-29 20:30:57 +0100

    create a basic menu

--- src/main.java
+++ src/main.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
+
 /**
  * Created by Albert Acerete on 29/11/16.
  * Lluís Bayer Soler
  */
 public class main {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Menú de Calculadora");
+        System.out.println("1. Funcionalidad pendiente");
+        int i = sc.nextInt();
+        switch(i){
+            case 1:
+                System.out.println("Funcionalidad pendiente");
+                break;
+        }
     }
 }
