8f2f435a40b6b92e97722520d1e77090baaf47a9
gfandos <gerard.fandos@gmail.com>
2016-11-29 20:36:37 +0100

    Division y resta realizadas

---
+++ src/app/Operaciones.java
@@ -0,0 +1,13 @@
+package app;
+
+public class Operaciones {
+
+    public double dividir(double a, double b) {
+        return a/b;
+    }
+
+    public double restar(double a, double b) {
+        return a-b;
+    }
+
+}
